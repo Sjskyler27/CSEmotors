b229c97389af8105bb80917b01031d8fbfab1b28
Skyler Simpson <sjskyler27@gmail.com>
2023-06-08 09:28:35 -0600

    added yaml file

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 
-app.get("/",function(req,res){
-  res.render("index", {title: "home"});
+app.get("/", function (req, res) {
+  res.render("index", { title: "home" });
 });
 
 app.listen(port, () => {
