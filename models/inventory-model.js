201300d9cf8779df8fa7947ebb0eada934d96350
Skyler Simpson <sjskyler27@gmail.com>
2023-06-24 18:40:22 -0600

    class view is now showing

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -3,8 +3,44 @@ const pool = require("../database/")
 /* ***************************
  *  Get all classification data
  * ************************** */
-async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+async function getClassifications() {
+  return await pool.query(
+    "SELECT * FROM public.classification ORDER BY classification_name"
+  )
 }
 
-module.exports = {getClassifications}
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`,
+      [classification_id]
+    )
+    return data.rows
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
+  }
+}
+
+async function getVehicleByInvId(inventory_id) {
+  try {
+    const data = await pool.query(
+      "SELECT * FROM public.inventory as i WHERE i.inv_id = $1",
+      [inventory_id]
+    )
+    return data.rows
+  } catch (error) {
+    console.error("getvehiclebyinvid error " + error)
+  }
+}
+
+module.exports = {
+  getClassifications,
+  getInventoryByClassificationId,
+  getVehicleByInvId,
+}
--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 const express = require("express");
+const utilities = require('./utilities/index')
 const env = require("dotenv").config();
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const baseController = require("./controllers/baseController")
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRouter")
 
 const port = process.env.PORT;
 const host = process.env.HOST;
 
 
 app.use(require("./routes/static"));
+app.use("/inv", utilities.handleErrors(inventoryRoute))
+
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout");
 
-app.get("/", baseController.buildHome);
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+app.use(async (req, res, next) =>{
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`);
